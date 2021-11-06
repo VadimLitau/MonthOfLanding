c96dcaadcc2145c104457221869eff4a95c309a5
Vadim Litau <vadimlitau@yandex.ru>
2021-11-06 21:43:43 +0600

    Смена галереи велосипедов

--- vendor/js/index.js
+++ vendor/js/index.js
@@ -1,3 +1,4 @@
+//Слайдеры
 let countPlace = 0;
 let countPlaceTitle = 0;
 let countPlaceIcon = 0;
@@ -51,4 +52,32 @@ document.querySelector('.places__btn_rigth').addEventListener('click', () => {
     sliderLineIcon.style.left = countPlaceIcon + 'px';
     sliderLineTitle.style.left = countPlaceTitle + 'px';
     sliderLine.style.left = countPlace + 'px';
+})
+
+//Велосипеды - смена галереи
+document.querySelector('#bikes-road').addEventListener('click', () => {
+    document.querySelector('#road').classList.add('bikes__gallery_active_road');
+    document.querySelector('#bikes-road').classList.add('bikes__head-road_active');
+    document.querySelector('#tt').classList.remove('bikes__gallery_active_tt');
+    document.querySelector('#grevel').classList.remove('bikes__gallery_active_grevel');
+    document.querySelector('#bikes-grevel').classList.remove('bikes__head-road_active');
+    document.querySelector('#bikes-tt').classList.remove('bikes__head-road_active');
+})
+
+document.querySelector('#bikes-grevel').addEventListener('click', () => {
+    document.querySelector('#grevel').classList.add('bikes__gallery_active_grevel');
+    document.querySelector('#bikes-grevel').classList.add('bikes__head-road_active');
+    document.querySelector('#tt').classList.remove('bikes__gallery_active_tt');
+    document.querySelector('#road').classList.remove('bikes__gallery_active_road');
+    document.querySelector('#bikes-road').classList.remove('bikes__head-road_active');
+    document.querySelector('#bikes-tt').classList.remove('bikes__head-road_active');
+})
+
+document.querySelector('#bikes-tt').addEventListener('click', () => {
+    document.querySelector('#tt').classList.add('bikes__gallery_active_tt');
+    document.querySelector('#bikes-tt').classList.add('bikes__head-road_active');
+    document.querySelector('#road').classList.remove('bikes__gallery_active_road');
+    document.querySelector('#grevel').classList.remove('bikes__gallery_active_grevel');
+    document.querySelector('#bikes-road').classList.remove('bikes__head-road_active');
+    document.querySelector('#bikes-grevel').classList.remove('bikes__head-road_active');
 })
